067758c91b17bd89e6075a9cca94c9122ddc5d53
Tolgahan Gümüş <adalix@Tolgahans-MacBook-Pro.local>
2024-10-17 15:05:27 +0300

    added: ball class and removed: unnecessary code

--- app.js
+++ app.js
@@ -1,99 +1,115 @@
 const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext("2d");    
+const ctx = canvas.getContext("2d");   
+
+canvas.width = innerWidth;
+canvas.height = innerHeight
+
+// const img = new Image();
+// img.src = 'saha_yatay.png';
+// img.style.transform =" rotate(90deg)";
 
 
-const minX = 120;
-const rangeX = 200;
-
-let p = 0;
-
-const img = new Image();
-img.src = 'saha_yatay.png';
-img.style.transform =" rotate(90deg)";
-
-
-const player = {
-    playerWidth : 20,
+let mouse = {
+    x : innerWidth / 2,
+    y : innerHeight / 2
 }
 
 
-class CreatePlayer {
-    constructor(x, y, w, ra) {
-        this.x = x,
-        this.y = y,
-        this.w = w,
-        this.ra = ra
-    }
 
+addEventListener('mousemove', (e)=>{
+        mouse.x = e.clientX,
+        mouse.y = e.clientY
+
+})
+
+function getDistance(x1,y1,x2,y2){
+    let xDist = x2 - x1
+    let yDist = y2 - y1
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
+
+class Ball{
+    constructor(){
+        this.x = mouse.x,
+        this.y = mouse.y,
+        this.radius = 10,
+        this.color = 'white'
+    }
     draw(){
         ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.arc(this.x, 100, player.playerWidth, 0, Math.PI*2);
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
         ctx.fill();
+    }
+    update(){
+        this.draw()
     }
 }
 
-function createPlayer(x, y, w, ra){
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.arc(x, 100, player.playerWidth, 0, Math.PI*2);
-    ctx.fill();
+class CreatePlayer {
+    constructor() {
+        this.x = Math.random() * innerWidth,
+        this.y = Math.random() * innerHeight,
+        this.w = 20,
+        this.radius = 20
+        this.color = 'red'
+    }
+    
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        // ctx.lineWidth = 2;
+        // ctx.strokeStyle = "black";
+        // ctx.font = "20px Arial";
+        // ctx.strokeText("23", this.x-10, this.y+6);
+        // ctx.stroke();
+    }
+    update(){
+        this.draw()
+    }
 }
-
-
-console.log(createPlayer(300,300, 20))
-animate()
-
-// ctx.beginPath();
-// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-// ctx.fillStyle = "red";
-// ctx.fill();
-// ctx.lineWidth = 4;
-// ctx.strokeStyle = "blue";
-// ctx.stroke();
+let ball  = new Ball()
+let player = new CreatePlayer(); 
 
 function animate(){
-    
-    let x = minX + rangeX * p;
-    let maxX = 500;
-
-    p = p + 0.05;
-
-
-    
-    if(x > canvas.width/2 - 120 ){
-            // x = 120;
-            return x;
-        }
-        
-        // ctx.clearRect(0,0,canvas.width, canvas.height);
-        ctx.drawImage(img, 0, 0, 1400,650 )
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.arc(x, 100, player.playerWidth, 0, Math.PI*2);
-        ctx.fill();
-
-
-        ctx.arc(x, 230, player.playerWidth, 0, Math.PI*2);
-
-        ctx.fillStyle = "white";
-        ctx.fill(); 
-
-        ctx.arc(x, 370, player.playerWidth, 0, Math.PI*2);
-        ctx.fill();
-
-        ctx.arc(x, 500, player.playerWidth, 0, Math.PI*2);
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.arc(x, 50, 30, 0, 2 * Math.PI);
-        ctx.fillStyle = "red";
-        ctx.fill();
-        ctx.lineWidth = 4;                      //! burasi tam istedigim gibi olusturacagin classi buna gore ayarla !!!
-        ctx.strokeStyle = "blue";
-        ctx.font = "25px Arial";
-        ctx.strokeText("23",x,50);
-        ctx.stroke();
 
     requestAnimationFrame(animate);
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    
+    ball.x = mouse.x
+    ball.y = mouse.y
+    ball.update()
+
+
+    player.x += 0.6 
+    player.y += 0.6 
+    player.update()
+
+    if(getDistance(ball.x, ball.y, player.x, player.y) < ball.radius + player.radius){
+        ball.color = 'red';
+    }else{
+        ball.color = 'white';
+    }
+
+    
+
 }
+animate()
+
+
+
+
+
+
+        // ctx.beginPath();
+        // ctx.arc(x, 50, 30, 0, 2 * Math.PI);
+        // ctx.fillStyle = "red";
+        // ctx.fill();
+        // ctx.lineWidth = 4;                      //! burasi tam istedigim gibi olusturacagin classi buna gore ayarla !!!
+        // ctx.strokeStyle = "blue";
+        // ctx.font = "25px Arial";
+        // ctx.strokeText("23",x,50);
+        // ctx.stroke();
